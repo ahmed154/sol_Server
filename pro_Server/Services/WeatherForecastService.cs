4545423ef8f310a44171dc0b1ba9e9449557580d
ahmed1542010 <ahmed1542010@gmail.com>
2020-07-01 23:58:11 +0300

    Add project files.

---
+++ pro_Server/Services/WeatherForecastService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using pro_Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace pro_Server.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly HttpClient httpClient;
+
+        public WeatherForecastService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<List<WeatherForecast>> GetWeatherForecasts()
+        {
+            return await httpClient.GetJsonAsync<List<WeatherForecast>>("weatherforecast");
+        }
+    }
+}
